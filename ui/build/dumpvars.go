9c9312b48e7bada234198860baf95e1e873b1687
bencetari <bence.tari@gmail.com>
2021-10-21 21:28:37 +0200

    Add Crystal variables to dumpvars
    Change-Id: Id434d432d1f7cc4db104fe2b9c562c2784f3bcca

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -137,6 +137,12 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
+	"CRYSTAL_VERSION",
+	"CRYSTAL_VERSION_NAME",
+	"CRYSTAL_MAINTAINER",
+	"CRYSTAL_PURITY",
+	"CRYSTAL_BUILD_TYPE",
+	"LINEBREAK",
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
 	"TARGET_PRODUCT",
@@ -166,6 +172,9 @@ func Banner(make_vars map[string]string) string {
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
+		}
+		if name == "LINEBREAK" {
+			fmt.Fprintf(b, "============================================\n")
 		}
 	}
 	fmt.Fprint(b, "============================================")
